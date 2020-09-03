bea0916a786e0a7b52d9ce91b896a3b3d284e5c9
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-09-03 18:46:19 -0400

    Fixed name of Procfile

--- src/components/JoinPage/index.js
+++ src/components/JoinPage/index.js
@@ -1,60 +1,20 @@
-import { Prompt, useParams } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { loadCredentials, saveCredentials } from "../../api/storage";
+import React from 'react'
+import { Client } from 'boardgame.io/react'
+import {Oversimplified} from '../../game/game'
+import {OversimplifiedBoard} from '../../game/board'
 
-import HelpButton from "../HelpButton";
-import HomeButton from "../HomeButton";
-import Multiplayer from "./Multiplayer";
-import { Player } from "../../logic";
-import ShareLink from "./ShareLink";
-import { joinMatch } from "../../api/match";
-
-const JoinPage = ({ serverURL }) => {
-  const { matchID, player } = useParams();
-  const [credentials, setCredentials] = useState("");
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const creds = loadCredentials(matchID, player);
-    if (creds) {
-      setCredentials(creds);
-    } else {
-      joinMatch(serverURL, matchID, player)
-        .then((creds) => {
-          saveCredentials(matchID, player, creds);
-          setCredentials(creds);
-        })
-        .catch((err) => setError(err.toString()));
-    }
-  }, [serverURL, matchID, player]);
-
-  return (
-    <div>
-      <div className="row flex-center">
-        <div className="col no-padding">
-          {credentials && (
-            <Multiplayer
-              serverURL={serverURL}
-              matchID={matchID}
-              player={player}
-              credentials={credentials}
-            />
-          )}
-          {error && (
-            <div className="alert alert-danger margin-top">{error}</div>
-          )}
-          {credentials && <ShareLink matchID={matchID} player={player} />}
-          <div className="row flex-center">
-            <div className="col no-padding">
-              <HelpButton />
-              <HomeButton />
-            </div>
-          </div>
-        </div>
-      </div>
-      <Prompt message="Are you sure you want to leave?" />
-    </div>
-  );
-};
-
-export default JoinPage;
+const App = Client({
+    // A game object.
+    game: Oversimplified,
+  
+    // The number of players.
+    numPlayers: 1,
+  
+    // Your React component representing the game board.
+    // The props that this component receives are listed below.
+    board: OversimplifiedBoard,
+    multiplayer: true,
+  
+    // Set to false to disable the Debug UI.
+    debug: false
+  });
