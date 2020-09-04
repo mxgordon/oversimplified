65c8e2026274bde4c3a71e01d95a08069084b80c
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-09-04 00:44:48 -0400

    plz work

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
 import {Server} from 'boardgame.io/server'
 import {Oversimplified} from "./game/game"
-// import path from 'path';
-// import serve from 'koa-static';
-
-// const server = Server({ games: [Oversimplified] });
-// const PORT = process.env.PORT || 8000;
-
-// // Build path relative to the server.js file
-// const frontEndAppBuildPath = path.resolve(__dirname, './build');
-// server.app.use(serve(frontEndAppBuildPath))
-
-// server.run(PORT, () => {
-//   server.app.use(
-//     async (ctx, next) => await serve(frontEndAppBuildPath)(
-//       Object.assign(ctx, { path: 'index.html' }),
-//       next
-//     )
-//   )
-// });
-
+import path from 'path';
+import serve from 'koa-static';
 
 const server = Server({ games: [Oversimplified] });
-const PORT = parseInt(process.env.PORT) || 8000;
+const PORT = process.env.PORT || 8000;
 
-server.run(PORT);
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath))
+
+server.run(PORT, () => {
+  server.app.use(
+    async (ctx, next) => await serve(frontEndAppBuildPath)(
+      Object.assign(ctx, { path: 'index.html' }),
+      next
+    )
+  )
+});
+
+
+// const server = Server({ games: [Oversimplified] });
+// const PORT = parseInt(process.env.PORT) || 8000;
+
+// server.run(PORT);
